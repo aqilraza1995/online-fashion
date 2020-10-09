ab6395a80dd6943f2894ff0870e1b3b22c9bd7da
aqil raza <aqilraza49@gmai.com>
2020-10-09 12:21:27 +0530

    Updates

---
+++ precache-manifest.53773cbf5346acfffcbc73e826d7c7fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cabcea182215d3b0d3903aeb456e844",
+    "url": "/online-fashion/index.html"
+  },
+  {
+    "revision": "90f6e6f179479a3906a0",
+    "url": "/online-fashion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28a6d833228b862182cc",
+    "url": "/online-fashion/static/js/2.626f2719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-fashion/static/js/2.626f2719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f6e6f179479a3906a0",
+    "url": "/online-fashion/static/js/main.1e24c951.chunk.js"
+  },
+  {
+    "revision": "e2da8112e618470bc0f7",
+    "url": "/online-fashion/static/js/runtime-main.50ff04e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/online-fashion/static/media/logo.5d5d9eef.svg"
+  }
+]);
