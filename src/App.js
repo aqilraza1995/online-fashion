ef0fc7b9d6d9b8a9dab49579cc9a0c87f60ca49f
aqil raza <aqilraza49@gmai.com>
2020-10-09 15:38:48 +0530

    line chnages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './homepage.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MAN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMAN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
